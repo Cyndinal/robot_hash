b8bc13b8c9fbc23be7c57dfdb942b28efebc9bbb
Max CYDIN <nyarkomaxwell03@gmail.com>
2024-05-17 23:30:56 +0000

    Changes in  the styling scheme

--- src/app/components/RobotPage.jsx
+++ src/app/components/RobotPage.jsx
@@ -6,21 +6,19 @@ import Card from "./Card";
 const RobotPage = ({robots})=>{
 
     return(
-        <>
 
-        <div className={'px-3 p-4 items-center justify-center grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4'}>
+
+        <div className={'overflow-y-auto h-80 px-3 p-4 items-center justify-center grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4'}>
             {robots.map((robot,index) => {
                 return(
                     <div key={index}
-                         className='bg-red-400 hover:bg-green-300 transition rounded-xl'>
+                         className='drop-shadow-md hover:drop-shadow-x rounded-xl transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300'>
                         <Card name={robots[index].name} email={robots[index].email}/>
                     </div>
                 )
             })}
         </div>
 
-
-        </>
     )
 
 
--- src/app/components/SearchBox.jsx
+++ src/app/components/SearchBox.jsx
@@ -4,19 +4,27 @@ const SearchBox = ({onSearchChange, SearchField}) => {
 
 
     return(
-        <>
-            <div className={'flex items-center justify-center mx-auto p-4 m-0.5 rounded-xl'}>
-                <input type={'search'}
-                       name={'robot'}
-                       value={SearchField}
-                       placeholder={'Search favorite Robot'}
-                       className={'outline-none p-4 shadow-amber-600'}
-                       onChange={onSearchChange}
-                />
-            </div>
+            <section className={'items-center justify-center py-1.5 px-3  bg-gradient-to-r from-white to-transparent z-50 w-full static mx-auto overflow-y-hidden'}>
+
+                <div
+                    className={'bg-gradient-to-bl from-indigo-600 to-indigo-400 text-white flex items-center justify-center font-sans mx-auto p-4 m-0.5  rounded-xl font-bold text-4xl lg:text-6xl '}>ROBOTS~TREND
+                </div>
+
+                <div className={'flex items-center justify-center mx-auto p-4 m-0.5 rounded-xl'}>
+                    <input type={'search'}
+                           name={'robot'}
+                           value={SearchField}
+                           placeholder={'Search favorite Robot'}
+                           className={'outline-none p-4  rounded-2xl '}
+
+                           onChange={onSearchChange}
+                    />
+                </div>
+
+            </section>
 
 
-        </>
+
     )
 }
 
